f265652ddd5b51ee01b12d695e3ed5a8c20d298a
cabrera-evil <douglascabrera.dev@gmail.com>
2023-01-23 17:36:25 -0600

    Delete: Trash folder

--- Linux/wsl.sh
+++
@@ -1,8 +0,0 @@
-#Updating System
-sudo sh ./config/update.sh
-#Install GCM
-sudo sh ./config/git/gcm.sh
-#Setting personal git account
-sudo sh ./config/git/account.sh
-#Install NPM Modules
-sudo sh ./app/npm.sh
