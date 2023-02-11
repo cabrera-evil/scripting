c8135d883d7b2e9c8a31e383ba20bd002b36412d
cabrera-evil <douglascabrera.dev@gmail.com>
2023-02-11 11:05:19 -0600

    Update: script output

--- docker/docker-desktop.sh
+++ docker/docker-desktop.sh
@@ -1,39 +1,65 @@
 #Install Kvm For Virtualization
+clear
+echo Installing KVM
 sudo apt -y install bridge-utils cpu-checker libvirt-clients libvirt-daemon qemu qemu-kvm
 kvm-ok
 #Update System Package
+clear
+echo Updating system packages
 sudo apt update && sudo apt upgrade -y
 [ -f /var/run/reboot-required ] && sudo reboot -f
 #Configure Kvm Support
+clear
+echo setting up KVM
 sudo modprobe kvm
 lsmod | grep kvm
 ls -al /dev/kvm
 #Add user to kvm
+clear
+echo Adding user to kvm
 sudo usermod -aG kvm $USER
 #Delete Old Installations
+clear
+echo Cleaning old instalations
 sudo apt remove docker-desktop
 rm -r $HOME/.docker/desktop
 sudo rm /usr/local/bin/com.docker.cli
 sudo apt purge docker-desktop
 #Docker Install
+clear
+echo Installing docker
 sudo apt install ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 #Setup The Repository
+clear
+echo Setting up docker repository
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 #Download A Test Version (Then You Can Update)
-wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.10.0-amd64.deb
+clear
+echo Downloading last version of docker desktop
+#wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.16.2-amd64.deb
 #Install The Downloaded Package
+clear
+echo Installing docker desktop
 sudo apt update
-sudo apt install ./docker-desktop-4.12.0-amd64.deb -y
+sudo apt install ./docker-desktop-4.16.2-amd64.deb -y
 #Cheking Version
+clear
+echo Checking docker version
 docker compose version
 docker --version
 docker version
 #Add User To Docker
+clear
+echo Adding user to docker organization
 sudo usermod -aG docker $USER
 #Enable Service Startup
+clear
+echo Enabling docker service
 systemctl --user enable docker-desktop
 #Open Docker-Desktop
+clear
+echo Opening docker desktop
 systemctl --user start docker-desktop
--- linux/app/npm.sh
+++ linux/app/npm.sh
@@ -1,13 +1,23 @@
 #Install NVM
+clear
+echo Installing NVM
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 #Exporting user config
+clear
+echo Exporting user config
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 #Reload config
+clear
+echo Realoading config
 source ~/.bashrc
 #Install NodeJs LTS
+clear
+echo Installing nodejs
 nvm i --lts
 #Install Modules
+clear
+echo Installing node modules
 npm i -g sass
 npm i -g nodemon
 npm i -g vite
