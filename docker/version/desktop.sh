2193704d3dc5317905f78e9a7cd216407103d131
Douglas Cabrera <84162354+cabrera-evil@users.noreply.github.com>
2023-02-27 11:05:36 -0600

    Update: docker desktop

--- docker/version/desktop.sh
+++ docker/version/desktop.sh
@@ -11,21 +11,25 @@ clear
 echo -e "${BLUE}Installing KVM${NC}"
 sudo apt -y install bridge-utils cpu-checker libvirt-clients libvirt-daemon qemu qemu-kvm
 kvm-ok
+
 #Update System Package
 clear
 echo -e "${BLUE}Updating system packages${NC}"
 sudo apt update && sudo apt upgrade -y
 [ -f /var/run/reboot-required ] && sudo reboot -f
+
 #Configure Kvm Support
 clear
 echo -e "${BLUE}Setting up KVM${NC}"
 sudo modprobe kvm
 lsmod | grep kvm
 ls -al /dev/kvm
+
 #Add user to kvm
 clear
 echo -e "${BLUE}Adding user to KVM${NC}"
 sudo usermod -aG kvm $USER
+
 #Delete Old Installations
 clear
 echo -e "${BLUE}Cleaning old installations${NC}"
@@ -33,43 +37,52 @@ sudo apt remove docker-desktop
 rm -r $HOME/.docker/desktop
 sudo rm /usr/local/bin/com.docker.cli
 sudo apt purge docker-desktop
+
 #Docker Install
 clear
 echo -e "${BLUE}Installing Docker${NC}"
 sudo apt install ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
 #Setup The Repository
 clear
 echo -e "${BLUE}Setting up Docker repository${NC}"
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
 #Download A Test Version (Then You Can Update)
 clear
 echo -e "${BLUE}Downloading latest version of Docker Desktop${NC}"
 wget -O /tmp/docker-desktop.deb "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.16.2-amd64.deb"
+
 #Install The Downloaded Package
 clear
 echo -e "${BLUE}Installing Docker Desktop${NC}"
 sudo apt update
 sudo dpkg -i /tmp/docker-desktop.deb
+
 # Deleting installation file
 echo -e "${GREEN}Deleting installation file${NC}"
 sudo rm /tmp/docker-desktop.deb
+
 #Cheking Version
 clear
 echo -e "${BLUE}Checking Docker version${NC}"
 docker compose version
 docker --version
 docker version
+
 #Add User To Docker
 clear
 echo -e "${BLUE}Adding user to Docker organization${NC}"
 sudo usermod -aG docker $USER
+
 #Enable Service Startup
 clear
 echo -e "${BLUE}Enabling Docker service${NC}"
 systemctl --user enable docker-desktop
+
 #Open Docker-Desktop
 clear
 echo -e "${BLUE}Opening Docker Desktop${NC}"
