a439d65b71bc5a2707f410cab7a8efd1d2da419c
cabrera-evil <douglascabrera.dev@gmail.com>
2023-02-26 19:13:21 -0600

    Update: linux scripts

--- linux/config/update.sh
+++ linux/config/update.sh
@@ -1,6 +1,28 @@
 #Updating System
+clear
+echo Updating System
 sudo apt-get update -y
+
+clear
+echo Upgrading Packages
 sudo apt-get upgrade -y
+
+clear
+echo Dist-Upgrade
 sudo apt-get dist-upgrade -y
+
+clear
+echo Full-Upgrade
 sudo apt-get full-upgrade -y
+
+clear
+echo Autoremove Packages
 sudo apt autoremove -y
+
+clear
+echo Autoclean Packages
+sudo apt autoclean -y
+
+clear
+echo Fixing Broken Packages
+sudo apt --fix-broken install -y
