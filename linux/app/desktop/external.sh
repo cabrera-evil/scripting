c6500e41822ba7221c8a922aa47488a6a1268292
cabrera-evil <douglascabrera.dev@gmail.com>
2023-02-28 21:50:34 -0600

    Add: brave browser for linux desktop

--- linux/app/desktop/external.sh
+++ linux/app/desktop/external.sh
@@ -47,3 +47,11 @@ if ! sudo rm /tmp/mongodb_compass.deb ; then
 fi
 
 echo -e "${green}MongoDB Compass installation complete!${NC}"
+
+# Install Brave Browser
+echo -e "${blue}Downloading Brave Browser...${NC}"
+sudo apt install apt-transport-https curl -y
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update -y
+sudo apt install brave-browser -y
