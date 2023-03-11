65a5cd3e2772d9d127785db7dc69be2415ce8de1
cabrera-evil <douglascabrera.dev@gmail.com>
2023-03-11 17:40:10 -0600

    Add: JDK 17

--- linux/app/terminal/dev.sh
+++ linux/app/terminal/dev.sh
@@ -11,16 +11,36 @@ clear
 echo -e "${YELLOW}Installing Proton dependencies...${NC}"
 sudo apt-get install -y libgbm-dev
 
+# Print success message
+echo -e "${GREEN}Proton dependencies and development tools installed successfully.${NC}"
+
 # Install basic development tools
 clear
 echo -e "${YELLOW}Installing Basic Development Tools...${NC}"
 sudo apt-get install build-essential -y
 
-# Install Java
+# Install JDK 17
 clear
-echo -e "${YELLOW}Installing Java...${NC}"
-sudo apt-get install default-jdk -y
-sudo apt-get install default-jre -y
+echo -e "${blue}Downloading JDK 17...${NC}"
+if ! wget -O /tmp/jdk-17_linux-x64_bin.deb "https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb"; then
+    echo -e "${red}Failed to download JDK 17.${NC}"
+    exit 1
+fi
 
-# Print success message
-echo -e "${GREEN}Proton dependencies and development tools installed successfully.${NC}"
+echo -e "${blue}Installing JDK 17 dependencies...${NC}"
+sudo apt-get install libc6-i386 libc6-x32 -y
+
+echo -e "${blue}Installing JDK 17...${NC}"
+if ! sudo dpkg -i /tmp/jdk-17_linux-x64_bin.deb; then
+    echo -e "${red}Failed to install JDK 17.${NC}"
+    exit 1
+fi
+
+echo -e "${green}JDK 17 installation complete!${NC}"
+
+# Config JDK-17 PATH
+echo -e "${blue}Configuring Java Path...${NC}"
+export JAVA_HOME=/usr/lib/jvm/jdk-17
+export PATH=$PATH:$JAVA_HOME/bin
+
+echo -e "${green}Java Path configured successfully!${NC}"
