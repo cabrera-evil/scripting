f79bc999c5247e68f7846e1d75d57b7400ba8316
cabrera-evil <douglascabrera.dev@gmail.com>
2023-04-19 17:16:43 -0600

    Update: install gns3

--- linux/app/desktop/gns3.sh
+++ linux/app/desktop/gns3.sh
@@ -10,7 +10,7 @@ NC='\e[0m' # No Color
 # Install GNS3
 clear
 echo -e "${BLUE}Installing GNS3...${NC}"
-sudo add-apt-repository ppa:gns3/ppa
+sudo add-apt-repository ppa:gns3/ppa -y
 sudo apt update                                
 sudo apt install gns3-gui gns3-server -y
 echo -e "${GREEN}GNS3 installed.${NC}"
