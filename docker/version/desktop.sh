cd2199074734f3bddd74722df03c695c2d3d66fd
cabrera-evil <douglascabrera.dev@gmail.com>
2023-04-27 00:15:09 -0600

    Update: terminal outputs

--- docker/version/desktop.sh
+++ docker/version/desktop.sh
@@ -13,25 +13,21 @@ sudo apt -y install bridge-utils cpu-checker libvirt-clients libvirt-daemon qemu
 kvm-ok
 
 #Update System Package
-clear
 echo -e "${BLUE}Updating system packages${NC}"
 sudo apt update && sudo apt upgrade -y
 [ -f /var/run/reboot-required ] && sudo reboot -f
 
 #Configure Kvm Support
-clear
 echo -e "${BLUE}Setting up KVM${NC}"
 sudo modprobe kvm
 lsmod | grep kvm
 ls -al /dev/kvm
 
 #Add user to kvm
-clear
 echo -e "${BLUE}Adding user to KVM${NC}"
 sudo usermod -aG kvm $USER
 
 #Delete Old Installations
-clear
 echo -e "${BLUE}Cleaning old installations${NC}"
 sudo apt remove docker-desktop
 sudo dpkg -r docker-desktop
@@ -40,42 +36,35 @@ sudo rm /usr/local/bin/com.docker.cli
 sudo apt purge docker-desktop
 
 #Docker Install
-clear
 echo -e "${BLUE}Installing Docker${NC}"
 sudo apt install ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 #Setup The Repository
-clear
 echo -e "${BLUE}Setting up Docker repository${NC}"
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
 #Download A Test Version (Then You Can Update)
-clear
 echo -e "${BLUE}Downloading latest version of Docker Desktop${NC}"
 wget -O /tmp/docker-desktop.deb "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.18.0-amd64.deb"
 
 #Install The Downloaded Package
-clear
 echo -e "${BLUE}Installing Docker Desktop${NC}"
 sudo apt-get update
 sudo dpkg -i /tmp/docker-desktop.deb
 
 #Cheking Version
-clear
 echo -e "${BLUE}Checking Docker version${NC}"
 docker compose version
 docker --version
 docker version
 
 #Add User To Docker
-clear
 echo -e "${BLUE}Adding user to Docker organization${NC}"
 sudo usermod -aG docker $USER
 
 #Open Docker-Desktop
-clear
 echo -e "${BLUE}Opening Docker Desktop${NC}"
 systemctl --user start docker-desktop
--- linux/config/update.sh
+++ linux/config/update.sh
@@ -13,31 +13,25 @@ echo -e "${BLUE}Updating System${NC}"
 sudo apt-get update -y
 
 # Upgrading packages
-clear
 echo -e "${BLUE}Upgrading Packages${NC}"
 sudo apt-get upgrade -y
 
 # Dist-upgrade
-clear
 echo -e "${BLUE}Dist-Upgrade${NC}"
 sudo apt-get dist-upgrade -y
 
 # Full-upgrade
-clear
 echo -e "${BLUE}Full-Upgrade${NC}"
 sudo apt-get full-upgrade -y
 
 # Autoremove packages
-clear
 echo -e "${BLUE}Autoremove Packages${NC}"
 sudo apt autoremove -y
 
 # Autoclean packages
-clear
 echo -e "${BLUE}Autoclean Packages${NC}"
 sudo apt autoclean -y
 
 # Fixing broken packages
-clear
 echo -e "${BLUE}Fixing Broken Packages${NC}"
 sudo apt --fix-broken install -y
