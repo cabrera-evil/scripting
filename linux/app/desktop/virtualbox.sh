767899f29a4953a2ac3628822be09483ce903948
cabrera-evil <douglascabrera.dev@gmail.com>
2023-05-10 21:40:23 -0600

    Add: virtualbox installation

---
+++ linux/app/desktop/virtualbox.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Colors for terminal output
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+YELLOW='\e[1;33m'
+BLUE='\e[0;34m'
+NC='\e[0m' # No Color
+
+# Download and install VirtualBox
+clear
+echo -e "${BLUE}Downloading VirtualBox...${NC}"
+if ! wget -O /tmp/virtualbox.deb "https://download.virtualbox.org/virtualbox/7.0.8/virtualbox-7.0_7.0.8-156879~Ubuntu~jammy_amd64.deb"; then
+    echo -e "${RED}Failed to download VirtualBox.${NC}"
+    exit 1
+fi
+
+echo -e "${BLUE}Installing VirtualBox...${NC}"
+if ! sudo dpkg -i /tmp/virtualbox.deb; then
+    echo -e "${RED}VirtualBox installed with some errors.${NC}"
+fi
+
+echo -e "${GREEN}VirtualBox installation complete!${NC}"
