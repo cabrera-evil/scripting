864313f3aa033dedfa00cfc542a4a975195c0634
cabrera-evil <douglascabrera.dev@gmail.com>
2023-05-23 20:39:50 -0600

    Add: jvm toolbox installation

--- linux/app/desktop/toolbox.sh
+++ linux/app/desktop/toolbox.sh
@@ -8,6 +8,37 @@ BLUE='\e[0;34m'
 NC='\e[0m' # No Color
 
 clear
+# Install JDK 17
+clear
+if [ -d "/usr/lib/jvm/jdk-17" ]; then
+    echo "JDK 17 already installed."
+else
+    echo -e "${BLUE}Downloading JDK 17...${NC}"
+    if ! wget -O /tmp/jdk-17_linux-x64_bin.deb "https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb"; then
+        echo -e "${RED}Failed to download JDK 17.${NC}"
+        exit 1
+    fi
+
+    echo -e "${BLUE}Installing JDK 17 dependencies...${NC}"
+    sudo apt-get install libc6-i386 libc6-x32 -y
+
+    echo -e "${BLUE}Installing JDK 17...${NC}"
+    if ! sudo dpkg -i /tmp/jdk-17_linux-x64_bin.deb; then
+        echo -e "${RED}Failed to install JDK 17.${NC}"
+        exit 1
+    fi
+
+    echo -e "${GREEN}JDK 17 installation complete!${NC}"
+
+    # Config JDK-17 PATH
+    echo -e "${BLUE}Configuring Java Path...${NC}"
+    export JAVA_HOME=/usr/lib/jvm/jdk-17
+    export PATH=$PATH:$JAVA_HOME/bin
+
+    echo -e "${green}Java Path configured successfully!${NC}"
+fi
+
+# Install Jetbrains Toolbox
 echo -e "${BLUE}Downloading JetBrains Toolbox App...${NC}"
 if ! wget -O /tmp/toolbox.tar.gz "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.28.1.15219.tar.gz?_gl=1*1a41i3s*_ga*MTQ0MTQ4MDkwMy4xNjg0NDcwMDA1*_ga_9J976DJZ68*MTY4NDQ3MDAwNS4xLjEuMTY4NDQ3MDEyMi4wLjAuMA.."; then
     echo -e "${RED}Failed to download JetBrains Toolbox App.${NC}"
