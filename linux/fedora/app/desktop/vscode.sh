0a1cc2ae74a09e7550961f2dff6947ddd68cbff8
cabrera-evil <douglascabrera.dev@gmail.com>
2023-05-29 19:11:40 -0600

    Feature: vscode from microsoft

--- linux/fedora/app/desktop/vscode.sh
+++ linux/fedora/app/desktop/vscode.sh
@@ -27,4 +27,17 @@ handle_error $? "echo" "Failed to add alias to ~/.bashrc."
 source ~/.bashrc
 handle_error $? "source" "Failed to reload terminal configuration."
 
-echo -e "${GREEN}Visual Studio Code alias added to ~/.bashrc and terminal configuration reloaded successfully!${NC}"
+# Download and install Visual Studio Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+handle_error $? "sudo rpm --import" "Failed to import Microsoft GPG key."
+
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+handle_error $? "sudo sh -c" "Failed to create vscode.repo file."
+
+sudo dnf check-update
+handle_error $? "sudo dnf check-update" "Failed to check for updates."
+
+sudo dnf install code -y
+handle_error $? "sudo dnf install" "Failed to install Visual Studio Code."
+
+echo -e "${GREEN}Visual Studio Code alias added to ~/.bashrc, terminal configuration reloaded, and Visual Studio Code installed successfully!${NC}"
