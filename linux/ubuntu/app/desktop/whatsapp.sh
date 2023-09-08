6fdd072d5e263f1a4d89017ccbd8c8acf7c5eb4a
cabrera-evil <douglascabrera.dev@gmail.com>
2023-09-07 20:38:17 -0600

    Update: flatpak apps

--- linux/ubuntu/app/desktop/whatsapp.sh
+++ linux/ubuntu/app/desktop/whatsapp.sh
@@ -19,6 +19,13 @@ handle_error() {
     fi
 }
 
+# If Flatpak is not installed, install it
+if ! [ -x "$(command -v flatpak)" ]; then
+    echo -e "${BLUE}Installing Flatpak...${NC}"
+    sudo apt install flatpak -y
+    handle_error $? "sudo apt install" "Failed to install Flatpak."
+fi
+
 # Install WhatsApp via Flatpak
 echo -e "${BLUE}Installing WhatsApp...${NC}"
 flatpak install flathub io.github.mimbrero.WhatsAppDesktop -y
