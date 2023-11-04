ba3551ff8592f6b10450236d87fd74ec02286508
cabrera-evil <douglascabrera.dev@gmail.com>
2023-11-03 23:30:36 -0600

    Fix: docker install

--- linux/debian/app/desktop/docker.sh
+++ linux/debian/app/desktop/docker.sh
@@ -20,41 +20,33 @@ handle_error() {
 
 #Delete Old Installations
 echo -e "${BLUE}Cleaning old installations${NC}"
-sudo apt remove docker-desktop
-sudo dpkg -r docker-desktop
+sudo apt remove docker-desktop -y
 rm -r $HOME/.docker/desktop
 sudo rm /usr/local/bin/com.docker.cli
-sudo apt purge docker-desktop
-
-#Apt Packages Install
-echo -e "${BLUE}Installing apt package index for HTTPS${NC}"
-sudo apt install ca-certificates curl gnupg -y
-handle_error $? "apt package" "Failed to install apt packages"
-
-#Setup GPG Key
-echo -e "${BLUE}Setting up GPG key${NC}"
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-handle_error $? "GPG Key Setup" "Failed to setup GPG key"
+sudo apt purge docker-desktop -y
 
 #Setup The Repository
 echo -e "${BLUE}Setting up Docker repository${NC}"
+# Add Docker's official GPG key:
+sudo apt update -y
+sudo apt install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-handle_error $? "Docker Repository Setup" "Failed to setup Docker repository"
-
-#Installing Docker Engine
-echo -e "${BLUE}Installing Docker Engine${NC}"
 sudo apt update -y
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-handle_error $? "Docker Engine Installation" "Failed to install Docker Engine"
 
 #Download Docker Desktop
 echo -e "${BLUE}Downloading latest version of Docker Desktop${NC}"
-wget -O /tmp/docker-desktop.deb "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.22.0-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64&_gl=1*n5tkzm*_ga*MzcxNjgxMTYxLjE2OTEzNTQ2MDQ.*_ga_XJWPQMJYHQ*MTY5MTM1NDYwMy4xLjEuMTY5MTM1NDYxMC41My4wLjA."
+download_url='https://desktop.docker.com/linux/main/amd64/docker-desktop-4.25.0-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64&_gl=1*1atagp5*_ga*OTA3OTE1NjExLjE2OTY3MTA0NDY.*_ga_XJWPQMJYHQ*MTY5OTA3NDM1NS4zLjEuMTY5OTA3NDM1Ny41OC4wLjA.'
+
+# # Download the package
+wget -O /tmp/docker-desktop.deb "$download_url"
 handle_error $? "Docker Desktop Download" "Failed to download Docker Desktop"
 
 #Install The Downloaded Package
