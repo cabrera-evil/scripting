32f8f5fbe8661fb7a2d0323b75ebe234f9071486
cabrera-evil <douglascabrera.dev@gmail.com>
2023-11-12 15:10:12 -0600

    Feat: grub config

--- linux/debian/config/grub.sh
+++ linux/debian/config/grub.sh
@@ -29,7 +29,24 @@ sudo sed -i 's/GRUB_DEFAULT=0/GRUB_DEFAULT=saved/g' /etc/default/grub
 handle_error $? "sed -i 's/GRUB_DEFAULT=0/GRUB_DEFAULT=saved/g' /etc/default/grub" "Failed to update GRUB configuration"
 
 # Add GRUB_SAVEDEFAULT to GRUB configuration just below GRUB_DEFAULT
-sudo sed -i '/GRUB_DEFAULT=saved/a GRUB_SAVEDEFAULT=true' /etc/default/grub
+echo -e "${YELLOW}Adding GRUB save default configuracion...${NC}"
+if ! grep -q 'GRUB_SAVEDEFAULT=true' /etc/default/grub; then
+    sudo sed -i '/GRUB_DEFAULT=saved/a GRUB_SAVEDEFAULT=true' /etc/default/grub
+    handle_error $? "sed -i '/GRUB_DEFAULT=saved/a GRUB_SAVEDEFAULT=true' /etc/default/grub" "Failed to add GRUB save default"
+else
+    echo -e "${YELLOW}GRUB save default already exists, skipping...${NC}"
+fi
+handle_error $? "sed -i '/GRUB_DEFAULT=saved/a GRUB_SAVEDEFAULT=true' /etc/default/grub" "Failed to add GRUB save default"
+
+# Set GRUB timeout to 5 seconds
+echo -e "${YELLOW}Setting GRUB timeout to 5 seconds...${NC}"
+sudo sed -i 's/GRUB_TIMEOUT=10/GRUB_TIMEOUT=5/g' /etc/default/grub
+handle_error $? "sed -i 's/GRUB_TIMEOUT=10/GRUB_TIMEOUT=5/g' /etc/default/grub" "Failed to update GRUB configuration"
+
+# Disable grub os-prober
+echo -e "${YELLOW}Disabling GRUB os-prober...${NC}"
+sudo sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
+handle_error $? "sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub" "Failed to update GRUB configuration"
 
 # Update GRUB configuration
 echo -e "${YELLOW}Updating GRUB configuration...${NC}"
