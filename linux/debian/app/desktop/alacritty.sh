58eaec7d0599daa877ab182b65736facbe208f7e
cabrera-evil <douglascabrera.dev@gmail.com>
2024-03-26 08:42:51 -0600

    feat: ohmybash script

--- linux/debian/app/desktop/alacritty.sh
+++ linux/debian/app/desktop/alacritty.sh
@@ -50,35 +50,4 @@ echo -e "${BLUE}Copying the bashrc file...${NC}"
 cp ~/.bashrc ~/.bashrc.bak
 handle_error $? "Copy the bashrc file" "Failed to copy the bashrc file"
 
-# Download NerdFonts
-echo -e "${BLUE}Downloading NerdFonts...${NC}"
-mkdir -p ~/.local/share/fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/CascadiaCode.zip -O /tmp/CascadiaCode.zip
-unzip /tmp/CascadiaCode.zip -d ~/.local/share/fonts
-handle_error $? "Download NerdFonts" "Failed to download NerdFonts"
-
-# Update font cache
-echo -e "${BLUE}Updating font cache...${NC}"
-fc-cache -f -v
-handle_error $? "Update font cache" "Failed to update font cache"
-
-# Install starship prompt
-echo -e "${BLUE}Installing starship prompt...${NC}"
-curl -fsSL https://starship.rs/install.sh | sh
-handle_error $? "Install starship prompt" "Failed to install starship prompt"
-
-# Add starship prompt to bashrc
-echo -e "${BLUE}Adding starship prompt to bashrc...${NC}"
-echo 'eval "$(starship init bash)"' >>~/.bashrc
-
-# Download Oh My Bash
-echo -e "${BLUE}Downloading Oh My Bash...${NC}"
-bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
-handle_error $? "Download Oh My Bash" "Failed to download Oh My Bash"
-
-# Add the old bashrc to the new bashrc
-echo -e "${BLUE}Adding the old bashrc to the new bashrc...${NC}"
-awk '/export NVM_DIR/,0' ~/.bashrc.bak >>~/.bashrc
-handle_error $? "Add the old bashrc to the new bashrc" "Failed to add the old bashrc to the new bashrc"
-
 echo -e "${GREEN}Alacritty has been installed successfully!${NC}"
