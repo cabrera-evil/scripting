bd7cd6936da90e1b0665e3c5bfa650efa96a8de2
Douglas Cabrera <douglascabrera.dev@gmail.com>
2024-05-21 09:50:13 -0600

    refactor: docker install with a newest and dynamic version

--- linux/debian/app/desktop/docker.sh
+++ linux/debian/app/desktop/docker.sh
@@ -8,26 +8,33 @@ NC='\033[0m' # No Color
 
 # Error handling function
 handle_error() {
-    local exit_code=$1
-    local command=$2
-    local message=$3
+  local exit_code=$1
+  local command=$2
+  local message=$3
 
-    if [ $exit_code -ne 0 ]; then
-        echo -e "${RED}Error: $command failed - $message${NC}" >&2
-        exit $exit_code
-    fi
+  if [ $exit_code -ne 0 ]; then
+    echo -e "${RED}Error: $command failed - $message${NC}" >&2
+    exit $exit_code
+  fi
 }
 
-#Delete Old Installations
+# Global Variables
+DOCKER_VERSION=4.30.0
+
+# Uninstall docker desktop
 echo -e "${BLUE}Cleaning old installations${NC}"
-sudo apt remove docker-desktop -y
+sudo apt purge --remove docker-desktop -y
 rm -r $HOME/.docker/desktop
 sudo rm /usr/local/bin/com.docker.cli
-sudo apt purge docker-desktop -y
 
-#Setup The Repository
+# Uninstall docker engine
+echo -e "${BLUE}Uninstalling Docker Engine${NC}"
+sudo apt purge --remove -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+sudo apt autoremove -y
+sudo apt autoclean -y
+
+# Setup The Repository
 echo -e "${BLUE}Setting up Docker repository${NC}"
-# Add Docker's official GPG key:
 sudo apt update -y
 sudo apt install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -37,31 +44,34 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 # Add the repository to Apt sources:
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt update -y
 
-#Installing Docker Engine
+# Installing Docker Engine
 echo -e "${BLUE}Installing Docker Engine${NC}"
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 handle_error $? "Docker Engine Installation" "Failed to install Docker Engine"
 
-#Download Docker Desktop
+# Download Docker Desktop
 echo -e "${BLUE}Downloading latest version of Docker Desktop${NC}"
-download_url='https://desktop.docker.com/linux/main/amd64/docker-desktop-4.25.0-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64&_gl=1*1atagp5*_ga*OTA3OTE1NjExLjE2OTY3MTA0NDY.*_ga_XJWPQMJYHQ*MTY5OTA3NDM1NS4zLjEuMTY5OTA3NDM1Ny41OC4wLjA.'
-
-# # Download the package
-wget -O /tmp/docker-desktop.deb "$download_url"
+wget -O /tmp/docker-desktop.deb "https://desktop.docker.com/linux/main/amd64/149282/docker-desktop-${DOCKER_VERSION}-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64"
 handle_error $? "Docker Desktop Download" "Failed to download Docker Desktop"
 
-#Install The Downloaded Package
+# Install The Downloaded Package
 echo -e "${BLUE}Installing Docker Desktop${NC}"
 sudo apt update -y
 sudo apt install -y /tmp/docker-desktop.deb
 handle_error $? "Docker Desktop Installation" "Failed to install Docker Desktop"
 
-#Add User To Docker
+# Add User To Docker
 echo -e "${BLUE}Adding user to Docker organization${NC}"
 sudo usermod -aG docker $USER
+newgrp docker
+
+# Enable the Docker service
+echo -e "${BLUE}Enabling Docker service${NC}"
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
 
 echo -e "${GREEN}Docker installation and configuration complete!${NC}"
