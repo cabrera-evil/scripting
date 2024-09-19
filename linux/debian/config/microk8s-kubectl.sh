fac72fa552dbd4373a3a0e9fba812ae82e6dc9d6
cabrera-evil <douglascabrera.dev@gmail.com>
2024-09-19 10:46:33 -0600

    chore(update): kubeconfig file name

--- linux/debian/config/microk8s-kubectl.sh
+++ linux/debian/config/microk8s-kubectl.sh
@@ -18,6 +18,6 @@ chmod 0700 $HOME/.kube
 
 # Export kubeconfig
 echo -e "${BLUE}Exporting kubeconfig...${NC}"
-microk8s kubectl config view --raw >$HOME/.kube/config
+microk8s kubectl config view --raw >$HOME/.kube/config-dev
 
 echo -e "${GREEN}Microk8s kubeconfig exported successfully!${NC}"
