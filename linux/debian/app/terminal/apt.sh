d999d76db9d61f444a6714e10c801d5284c4bcd8
cabrera-evil <douglascabrera.dev@gmail.com>
2024-09-24 10:13:58 -0600

    chore(update): recursive installs

--- linux/debian/app/terminal/apt.sh
+++ linux/debian/app/terminal/apt.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
 # Colors for terminal output
-RED='\e[0;31m'
-GREEN='\e[0;32m'
-YELLOW='\e[1;33m'
 BLUE='\e[0;34m'
+GREEN='\e[0;32m'
+RED='\e[0;31m'
 NC='\e[0m' # No Color
 
-# Install packages
-echo -e "${BLUE}Installing packages...${NC}"
-sudo apt update -y
-sudo apt install -y htop neofetch git git-flow vim build-essential xclip unzip unrar tmux ncdu trash-cli ranger zsh bluez rsync
+# Packages to install
+packages=(
+    bat
+    bluez
+    build-essential
+    curl
+    git
+    git-flow
+    htop
+    lsd
+    ncdu
+    neofetch
+    ranger
+    resolvconf
+    rsync
+    tmux
+    trash-cli
+    unzip
+    unrar
+    vim
+    wireguard
+    xclip
+    zsh
+    zip
+    wget
+)
+
+# Update and install packages
+echo -e "${BLUE}Updating package lists and installing packages...${NC}"
+sudo apt update -y && sudo apt install -y "${packages[@]}"
 
 echo -e "${GREEN}Packages installation complete!${NC}"
--- linux/debian/config/microk8s-plugins.sh
+++ linux/debian/config/microk8s-plugins.sh
@@ -7,12 +7,28 @@ YELLOW='\e[1;33m'
 BLUE='\e[0;34m'
 NC='\e[0m' # No Color
 
+# Plugins to enable (separated by spaces)
+plugins=(
+    dns
+    dashboard
+    helm
+    hostpath-storage
+    ingress
+)
+
 # Wait for microk8s to start
 echo -e "${BLUE}Waiting for microk8s to start...${NC}"
 microk8s status --wait-ready
 
 # Enable microk8s plugins
 echo -e "${BLUE}Enabling microk8s plugins...${NC}"
-microk8s enable dns dashboard helm hostpath-storage ingress
+for plugin in "${plugins[@]}"; do
+    if microk8s enable "$plugin"; then
+        echo -e "${GREEN}$plugin enabled successfully!${NC}"
+    else
+        echo -e "${RED}Failed to enable $plugin${NC}"
+        exit 1
+    fi
+done
 
-echo -e "${GREEN}Microk8s plugins enabled successfully!${NC}"
+echo -e "${GREEN}All Microk8s plugins enabled successfully!${NC}"
