27d94545d408ccf71ba66d06b52d84a85c285566
cabrera-evil <douglascabrera.dev@gmail.com>
2024-09-26 10:05:09 -0600

    refactor: rancher install with helm charts

--- linux/debian/app/terminal/rancher.sh
+++ linux/debian/app/terminal/rancher.sh
@@ -7,17 +7,14 @@ YELLOW='\e[1;33m'
 BLUE='\e[0;34m'
 NC='\e[0m' # No Color
 
-echo -e "${BLUE}Installing Rancher...${NC}"
+echo -e "${BLUE}Installing Rancher helm chart...${NC}"
 helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
-
-echo -e "${BLUE}Creating cattle-system namespace...${NC}"
-kubectl create namespace cattle-system
-
-echo -e "${BLUE}Installing cert-manager...${NC}"
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.3/cert-manager.crds.yaml
 
 echo -e "${BLUE}Installing cert-manager helm chart...${NC}"
 helm repo add jetstack https://charts.jetstack.io
+
+echo -e "${BLUE}Creating cattle-system namespace...${NC}"
+kubectl create namespace cattle-system
 
 echo -e "${BLUE}Updating helm repositories...${NC}"
 helm repo update
@@ -25,6 +22,12 @@ helm repo update
 echo -e "${BLUE}Installing cert-manager...${NC}"
 helm install cert-manager jetstack/cert-manager \
     --namespace cert-manager \
-    --create-namespace
+    --create-namespace \
+    --version v1.5.3
 
 echo -e "${BLUE}Installing Rancher...${NC}"
+helm install rancher rancher-latest/rancher \
+    --namespace cattle-system \
+    --set hostname=rancher.sslip.io \
+    --set replicas=1 \
+    --set bootstrapPassword=admin
