b93b00f86b517066bd651ec87a2c6012200e6845
cabrera-evil <douglascabrera.dev@gmail.com>
2024-12-24 22:15:34 -0600

    refactor: docker gpg key

--- linux/debian/app/desktop/docker.sh
+++ linux/debian/app/desktop/docker.sh
@@ -9,30 +9,18 @@ NC='\033[0m' # No Color
 # Define variables
 URL="https://desktop.docker.com/linux/main/amd64/167172/docker-desktop-amd64.deb"
 
-# Uninstall docker desktop
-echo -e "${BLUE}Cleaning old installations${NC}"
-sudo apt purge --remove docker-desktop -y
-rm -r $HOME/.docker/desktop
-sudo rm /usr/local/bin/com.docker.cli
-
-# Uninstall docker engine
-echo -e "${BLUE}Uninstalling Docker Engine${NC}"
-sudo apt purge --remove -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
-sudo apt autoremove -y
-sudo apt autoclean -y
-
 # Setup The Repository
 echo -e "${BLUE}Setting up Docker repository${NC}"
 sudo apt update -y
 sudo apt install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
   sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt update -y
 
