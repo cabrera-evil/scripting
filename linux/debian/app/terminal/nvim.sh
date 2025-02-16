e9a48c98407c454deae9b908a59872825cce8454
cabrera-evil <douglascabrera.dev@gmail.com>
2025-02-16 17:16:28 -0600

    feat: obsidian install and nvim updated repo

--- linux/debian/app/terminal/nvim.sh
+++ linux/debian/app/terminal/nvim.sh
@@ -8,33 +8,22 @@ BLUE='\e[0;34m'
 NC='\e[0m' # No Color
 
 # Define variables
-URL="https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
+URL="https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz"
 
-# Downlaod nvim pre-built binary
+# Download nvim pre-built binary
 echo -e "${BLUE}Downloading nvim pre-built binary...${NC}"
-wget -O /tmp/nvim-linux64.tar.gz "$URL"
+wget -O /tmp/nvim-linux-x86_64.tar.gz "$URL"
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf /tmp/nvim-linux64.tar.gz
+sudo tar -C /opt -xzf /tmp/nvim-linux-x86_64.tar.gz
 
 # Create symbolic link
 echo -e "${BLUE}Creating symbolic link...${NC}"
-sudo ln -sf /opt/nvim-linux64/bin/nvim /usr/bin/nvim
-
-# Add nvim path to environment
-echo -e "${BLUE}Adding nvim path to environment...${NC}"
-echo "export PATH=\$PATH:/opt/nvim-linux64/bin" >>~/.bashrc
-
-# Export nvim alias to bashrc (overwrite the vim alias)
-echo -e "${BLUE}Exporting nvim alias to bashrc...${NC}"
-if ! grep -q "alias vim=\"nvim\"" ~/.bashrc; then
-    echo 'alias vim="nvim"' >>~/.bashrc
-fi
-if ! grep -q "alias vi=\"nvim\"" ~/.bashrc; then
-    echo 'alias vi="nvim"' >>~/.bashrc
-fi
+sudo ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
 
 # Install NvChad
 echo -e "${BLUE}Installing NvChad...${NC}"
-git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+if [ ! -d ~/.config/nvim ]; then
+    git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+fi
 
 echo -e "${GREEN}Nvim has been installed successfully!${NC}"
