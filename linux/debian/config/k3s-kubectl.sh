0baeb18027121d378947406e355227cebb2bd4bd
cabrera-evil <douglascabrera.dev@gmail.com>
2025-04-05 23:11:34 -0600

    refactor: k3s config for local development

--- linux/debian/config/k3s-kubectl.sh
+++ linux/debian/config/k3s-kubectl.sh
@@ -10,14 +10,18 @@ NC='\e[0m' # No Color
 # Create K3s cache dir
 echo -e "${BLUE}Creating k3s cache dir...${NC}"
 mkdir -p $HOME/.kube
-chmod 700 $HOME/.kube
+chmod 0600 $HOME/.kube
 
 # Export kubeconfig
 echo -e "${BLUE}Exporting kubeconfig...${NC}"
-sudo cat /etc/rancher/k3s/k3s.yaml | sudo tee $HOME/.kube/config-k3s-local > /dev/null
+sudo cat /etc/rancher/k3s/k3s.yaml | sudo tee $HOME/.kube/config-local > /dev/null
+
+# Change context to local
+echo -e "${BLUE}Changing context from 'default' to 'local'...${NC}"
+sed -i 's/default/local/g' $HOME/.kube/config-local
 
 # Change ownership of kubeconfig
 echo -e "${BLUE}Changing ownership of kubeconfig...${NC}"
-sudo chown $(id -u):$(id -g) $HOME/.kube/config-k3s-local
+sudo chown $(id -u):$(id -g) $HOME/.kube/config-local
 
 echo -e "${GREEN}K3s kubeconfig exported successfully!${NC}"
