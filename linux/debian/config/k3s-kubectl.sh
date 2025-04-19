17d9fd026ffd8f6449e80fa51167a8cbbcb62e10
cabrera-evil <douglascabrera.dev@gmail.com>
2025-04-18 21:37:51 -0600

    fix: k3s kube permissions

--- linux/debian/config/k3s-kubectl.sh
+++ linux/debian/config/k3s-kubectl.sh
@@ -10,7 +10,7 @@ NC='\e[0m' # No Color
 # Create K3s cache dir
 echo -e "${BLUE}Creating k3s cache dir...${NC}"
 mkdir -p $HOME/.kube
-chmod 0600 $HOME/.kube
+chmod 700 $HOME/.kube
 
 # Export kubeconfig
 echo -e "${BLUE}Exporting kubeconfig...${NC}"
@@ -18,7 +18,7 @@ sudo cat /etc/rancher/k3s/k3s.yaml | sudo tee $HOME/.kube/config-local > /dev/nu
 
 # Change context to local
 echo -e "${BLUE}Changing context from 'default' to 'local'...${NC}"
-sed -i 's/default/local/g' $HOME/.kube/config-local
+sudo sed -i 's/default/local/g' $HOME/.kube/config-local
 
 # Change ownership of kubeconfig
 echo -e "${BLUE}Changing ownership of kubeconfig...${NC}"
